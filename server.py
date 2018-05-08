938541ff48da2db1fc9f3c6e91ee5b974bd34aec
Octavia Payne <oapayne@gmail.com>
2018-05-08 12:47:38 -0700

    set up addition configurations to make flask app run

--- server.py
+++ server.py
@@ -1,9 +1,20 @@
-from flask import Flask, sessoin, render_template, request, flash, redirect
-from flask_debugtoolbar import DebugToolBarExtension
+from flask import Flask, session, render_template, request, flash, redirect
+from flask_debugtoolbar import DebugToolbarExtension
 from model import *
 
 app = Flask(__name__)
+app.secret_key = "athena"
+
+@app.route("/")
+def welcome_user():
+
+	return "Welcome!"
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+	# Use the DebugToolbar
+	DebugToolbarExtension(app)
+
+	app.run(debug=True, host="0.0.0.0")
