f755f46b63f5ba76c9ed97b9aa40e7fb441c4a72
Octavia Payne <oapayne@gmail.com>
2018-05-08 15:39:56 -0700

    connected server to db, added login validation, added flash message when login info not found in db.

--- server.py
+++ server.py
@@ -8,11 +8,45 @@ app.secret_key = "athena"
 @app.route("/")
 def welcome_user():
 
-	return render_template("index.html")
+	return render_template("index.html", session=session)
 
+@app.route("/login")
+def display_login():
+
+	return render_template("login.html")
+
+@app.route("/handle-login", methods=['POST'])
+def handle_login():
+	"""Validate user info and save username in session."""
+	
+	# get username from form submission
+	username = request.form['username']
+	pw = request.form['password']
+
+	# check if username and pw exist in db
+	if User.query.filter_by(username=username, password=pw).all():
+		session['username'] = username
+		return redirect("/")
+	else:
+		flash('Email and/or password is invalid. Please try again.')
+		return redirect("/login")
+
+@app.route("/handle-logout")
+def handle_logout():
+
+	session.clear()
+
+	return redirect("/")
+
+@app.route("/signup")
+def display_signup():
+
+	return render_template("signup.html")
 
 if __name__ == "__main__":
-	app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+	# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+	connect_to_db(app)
 
 	# Use the DebugToolbar
 	DebugToolbarExtension(app)
