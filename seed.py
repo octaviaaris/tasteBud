4ada85444ee9ae685d24c1deb010cd3c3e4e657c
Octavia Payne <oapayne@gmail.com>
2018-05-08 11:55:57 -0700

    changed function names to seed_* and changed seed_restaurants() to ensure no duplication before adding instance to db

--- seed.py
+++ seed.py
@@ -3,7 +3,7 @@ from model import *
 
 init_app()
 
-def create_restaurant(filename):
+def seed_restaurants(filename):
 	"""Import restaurants from data file."""
 
 	with open(filename) as f:
@@ -19,8 +19,8 @@ def create_restaurant(filename):
 			city = line["location"]['city']
 			state = line["location"]['state']
 			zipcode = line["location"]['zip_code']
-			# latitude = line['coordinates']['latitude']
-			# longitude = line['coordinates']['longitude']
+			latitude = line['coordinates']['latitude']
+			longitude = line['coordinates']['longitude']
 
 
 			new = Restaurant(restaurant_id=restaurant_id,
@@ -32,17 +32,45 @@ def create_restaurant(filename):
 							 city=city,
 							 state=state,
 							 zipcode=zipcode,
-							 # latitude=latitude,
-							 # longitude=longitude,
-							 # hours=None,
-							 price=None)
+							 latitude=latitude,
+							 longitude=longitude,
+							 hours=None,
+							 price=None,
+							 yelp_rating=None)
 
-			db.session.add(new)
+			if not Restaurant.query.filter_by(restaurant_id=restaurant_id).all():
+				db.session.add(new)
 
 		db.session.commit()
 
 
-def create_price():
+def seed_categories(filename):
+	"""Import categories from data file."""
+
+	aliases = []
+
+	with open(filename) as f:
+		for line in f:
+			line = json.loads(line)
+			categories = line['categories']
+			
+			for c in categories:
+				alias = c['alias']
+
+				# ensure no duplicates
+				if alias not in aliases:
+					aliases.append(alias)
+
+
+	for a in aliases:
+		if not Category.query.filter_by(alias=a).all():
+			new = Category(alias=a)
+			db.session.add(new)
+
+	db.session.commit()
+
+
+def seed_prices():
 	"""Populate price table."""
 
 	one = Price(price_id=1)
@@ -54,26 +82,3 @@ def create_price():
 	db.session.commit()
 
 
-def create_categories(filename):
-	"""Import categories from data file."""
-
-	aliases = []
-
-	with open(filename) as f:
-		for line in f:
-			line = json.loads(line)
-			categories = line['categories']
-				
-			for c in categories:
-				alias = c['alias']
-
-				if alias not in aliases:
-					aliases.append(alias)
-
-
-	for a in aliases:
-		if not Category.query.filter_by(alias=a).all():
-			new = Category(alias=a)
-			db.session.add(new)
-
-	db.session.commit()
