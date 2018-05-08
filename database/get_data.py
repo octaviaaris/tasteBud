3e1218468eab70159790cf0e1090aafc0df630c3
Octavia Payne <oapayne@gmail.com>
2018-05-08 12:07:06 -0700

    moved all db files to directory

---
+++ database/get_data.py
@@ -0,0 +1,68 @@
+from urllib2 import HTTPError
+from urllib import quote
+from urllib import urlencode
+import os
+import requests
+import json
+
+API_KEY = os.environ['YELP_API_KEY']
+
+API_HOST = 'https://api.yelp.com'
+SEARCH_PATH = '/v3/businesses/search'
+BUSINESS_PATH = '/v3/businesses/'  # Business ID will come after slash.
+HEADER = {'Authorization': 'Bearer {key}'.format(key=API_KEY)}
+
+
+def search_businesses(location, filename, offset=0):
+	"""Get info about businesses from specified location.
+	
+	-setting term to restaurant filters out non-food businesses
+	-offset gets the next batch to avoid repeats
+	-limit specifies how many results to return with each query
+
+	"""
+
+	url = "https://api.yelp.com/v3/businesses/search"
+
+	params = {"term": "restaurant",
+			  "location": location,
+			  "offset": offset,
+			  "limit": 50}
+
+	response = requests.request("GET", url, headers=HEADER, params=params)
+
+	result = response.json()
+
+	businesses = result["businesses"]
+
+	with open(filename, "a+") as f:
+		string = f.read()
+		for b in businesses:
+			# ensure entry is not a repeat
+			if b['id'] not in string:
+				f.write(json.dumps(b))
+				f.write("\n")
+
+
+def get_sf(filename):
+	"""Get 1000 restaurants in SF."""
+
+	offset = 0
+
+	while offset < 951:
+		search_businesses('San Francisco', filename, offset)
+		offset += 50
+
+	return "Done!"
+
+
+def get_oak(filename):
+	"""Get 1000 restaurants in Oakland."""
+
+	offset = 0
+
+	while offset < 951:
+		search_businesses('Oakland', filename, offset)
+		offset += 50
+
+	return "Done!"
---
+++ database/seed.py
@@ -0,0 +1,84 @@
+import json
+from model import *
+
+init_app()
+
+def seed_restaurants(filename):
+	"""Import restaurants from data file."""
+
+	with open(filename) as f:
+		for line in f:
+			line = json.loads(line)
+
+			restaurant_id = line['id']
+			name = line['name']
+			url = line['url']
+			address1 = line["location"]['address1']
+			address2 = line["location"]['address2']
+			address3 = line["location"]['address3']
+			city = line["location"]['city']
+			state = line["location"]['state']
+			zipcode = line["location"]['zip_code']
+			latitude = line['coordinates']['latitude']
+			longitude = line['coordinates']['longitude']
+
+
+			new = Restaurant(restaurant_id=restaurant_id,
+							 name=name,
+							 url=url,
+							 address1=address1,
+							 address2=address2,
+							 address3=address3,
+							 city=city,
+							 state=state,
+							 zipcode=zipcode,
+							 latitude=latitude,
+							 longitude=longitude,
+							 hours=None,
+							 price=None,
+							 yelp_rating=None)
+
+			if not Restaurant.query.filter_by(restaurant_id=restaurant_id).all():
+				db.session.add(new)
+
+		db.session.commit()
+
+
+def seed_categories(filename):
+	"""Import categories from data file."""
+
+	aliases = []
+
+	with open(filename) as f:
+		for line in f:
+			line = json.loads(line)
+			categories = line['categories']
+			
+			for c in categories:
+				alias = c['alias']
+
+				# ensure no duplicates
+				if alias not in aliases:
+					aliases.append(alias)
+
+
+	for a in aliases:
+		if not Category.query.filter_by(alias=a).all():
+			new = Category(alias=a)
+			db.session.add(new)
+
+	db.session.commit()
+
+
+def seed_prices():
+	"""Populate price table."""
+
+	one = Price(price_id=1)
+	two = Price(price_id=2)
+	three = Price(price_id=3)
+	four = Price(price_id=4)
+
+	db.session.add_all([one, two, three, four])
+	db.session.commit()
+
+
