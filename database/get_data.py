b7655e2fcba904ce193e39e17e7ac5e6619f0b62
Octavia Payne <oapayne@gmail.com>
2018-05-09 08:48:07 -0700

    began writing script to search businesses by id for ratings and prices

--- database/get_data.py
+++ database/get_data.py
@@ -66,3 +66,17 @@ def get_oak(filename):
 		offset += 50
 
 	return "Done!"
+
+
+def restaurant_price_and_rating():
+	"""Get prices for each restaurant."""
+
+	all_restaurants = Restaurant.query.all()
+
+	i = 1
+
+	for a in all_restaurants:
+		print "{i}: {id}".format(i=i, id=a.restaurant_id)
+		i += 1
+
+		# query Yelp using business ID
