a6684e5bfc482d34fd957ec410ce8b2187d55e58
Octavia Payne <oapayne@gmail.com>
2018-05-18 13:50:32 -0700

    file to set up testdb

---
+++ seed_testdb.py
@@ -0,0 +1,27 @@
+from server import app
+from model import db, connect_to_db, example_users, example_ratings
+from seed import (seed_prices,
+				  seed_categories,
+				  seed_restaurants,
+				  seed_price_ratings,
+				  seed_rest_cats,
+				  seed_users,
+				  seed_ratings)
+
+connect_to_db(app, 'postgres:///testdb')
+
+# create tables
+db.create_all()
+
+# seed testdb
+seed_prices()
+seed_categories('database/categories.txt')
+seed_restaurants('database/all_restaurants.txt')
+seed_price_ratings('database/price_ratings.txt')
+seed_rest_cats('database/all_restaurants.txt')
+seed_users('database/users.csv')
+seed_ratings('database/ratings.csv')
+
+# create test users and ratings
+example_users()
+example_ratings()
