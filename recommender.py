03cc9147be71096d61b48af53a23b99f33eea0b3
Octavia Payne <oapayne@gmail.com>
2018-05-20 12:47:09 -0700

    fixed get_new_restaurant_recs() to filter out already rated restaurants

--- recommender.py
+++ recommender.py
@@ -6,34 +6,34 @@ init_app()
 def get_new_restaurant_recs(user):
 	"""Finds restaurants based on restaurants similar to what user has rated 4 or above."""
 
-	# get restaurants from user's ratings where user's rating is 4 or above
-	top_rated = [r.restaurants for r in user.ratings if r.user_rating >= 4]
-	sim_restaurants = []
-	rated_restaurants = [r.restaurants for r in user.ratings]
-	new_restaurants = []
-	
 
-	# get similar restaurants for each restaurant in top_rated
-	for t in top_rated:
-		sim_restaurants += t.find_sim_restaurants()
+	# get restaurants from user's ratings where user's rating is 4 or above
+	user_favorites = [r.restaurants for r in user.ratings if r.user_rating >= 4]
+	
+	sim_restaurants = []
+	
+	# get similar restaurants for each restaurant in top_rated; append as (match score, r)
+	for restaurant in user_favorites:
+		sim_restaurants += restaurant.find_sim_restaurants()
+	
+	# all restaurants rated by user
+	user_rated = [r.restaurants for r in user.ratings]
+
+	new_restaurants = []
 
 	# filters out restaurants user has already rated and duplicates
 	for s in sim_restaurants:
-		if s[1].ratings not in rated_restaurants and s[1] not in new_restaurants:
+		if s[1] not in user_rated:
 			new_restaurants.append(s[1])
 
-	return new_restaurants
+	return list(set(new_restaurants))
 
 def show_top_picks(user):
 	"""Uses restaurant and user similarity recommendations to create a top 5 picks list."""
 
 	top_picks = []
 
-	user_recs = user.get_user_recs()
-	rest_sim = get_new_restaurant_recs(user)
+	top_picks += user.get_user_recs()
+	top_picks += get_new_restaurant_recs(user)
 
-	if user_recs and rest_sim:
-		top_picks += sample(user_recs, 1)
-		top_picks += sample(rest_sim, 4)
-	
-	return top_picks
+	return sample(top_picks, 5)
