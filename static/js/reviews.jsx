8dc207b5e6438821b89040b3c3f1d13f4839c317
Octavia Payne <oapayne@gmail.com>
2018-05-25 13:15:01 -0700

    completed reviews component

--- static/js/reviews.jsx
+++ static/js/reviews.jsx
@@ -1,8 +1,39 @@
 class UserReviews extends React.Component {
 	constructor(props) {
 		super(props)
-		this.state = {reviews: []};
+		this.state = {reviews: {}};
 	}
 
+	componentDidMount() {
+
+		fetch('/reviews.json',
+			  {credentials: 'include'}).then((response) => response.json())
+									   .then((data) => this.setState({reviews: data}));
+	}
 	
+	render() {
+
+		let url = "/details/"
+		let reviewArray = []
+		let reviewKey = 0
+
+		for (let review in this.state.reviews) {
+			reviewKey++;
+			reviewArray.push(
+				<p key={reviewKey}><a href={url + review} target="_blank">{this.state.reviews[review][0]}</a> | Price: {this.state.reviews[review][1]} | Your review: {this.state.reviews[review][2]}</p>
+				)
+		}
+
+		return (
+			<div>
+				<h2>Reviews</h2>
+				{reviewArray}
+			</div>
+			)
+	}
 }
+
+ReactDOM.render(
+	<UserReviews />,
+	document.getElementById("reviews")
+);
