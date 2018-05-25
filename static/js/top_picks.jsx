215ce4d7894163d7bac0bea60495f3247cdf9090
Octavia Payne <oapayne@gmail.com>
2018-05-24 18:05:47 -0700

    fixed call to /top-picks.json

--- static/js/top_picks.jsx
+++ static/js/top_picks.jsx
@@ -1,30 +1,44 @@
 class TopPicks extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {topPicks: []};
+		this.state = {topPicks: {}};
 	}
 
 	componentDidMount() {
-		fetch(`/top-picks.json`).then((response) => response.json())
-								.then((data) => {
-									let recs = []
-									for (let rec of data.recs) {
-										recs.push(rec)
-									}
-									this.setState({topPicks: recs});
-								});
 
-		console.log(this.state.topPicks);
+		$.get("/top-picks.json", 
+
+			function(result) {
+				this.setState({topPicks: result});
+				}.bind(this)
+			)
+
+		// look up how to send cookies with fetch
+		// fetch(`/top-picks.json`).then((response) => response.json())
+		// 						.then((data) => this.setState({topPicks: data}));
+
+		// console.log(this.state.topPicks);
+
 	}
 
 	render() {
 
-		// console.log(this.sate.topPicks);
-		return ("hi")
+		let url = "/details/"
+		let resultArray = [];
+		let resultKey = 0;
+
+		for (let pick in this.state.topPicks) {
+			resultKey++;
+			resultArray.push(
+				<li key={resultKey}><a href={url + pick} target="_blank">{this.state.topPicks[pick][0]}</a> ({this.state.topPicks[pick][1]})</li>
+				)
+		}
+		
+		return (<ul>{resultArray}</ul>)
 	}
 }
 
 ReactDOM.render(
 	<TopPicks />,
 	document.getElementById("recommendations")
-	)
+);
