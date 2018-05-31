63cafe083f961cca9df92b92d690d2d78a1292c2
Octavia Payne <oapayne@gmail.com>
2018-05-31 00:20:43 -0700

    implemented fetch; need to connect request with db

--- static/js/rateButtons.jsx
+++ static/js/rateButtons.jsx
@@ -3,24 +3,35 @@ class RateButton extends React.Component {
 		super(props);
 		this.state = {rating: 0};
 		this.rate = this.rate.bind(this);
+		this.changeRating = this.changeRating.bind(this);
+	}
+
+	changeRating() {
+
+		// POST request
+		// let data = new FormData();
+		// data.append("json", JSON.stringify({'rating': this.state.rating}));
+
+		// fetch('/rate-restaurant.json',
+		// 	  {credentials: 'include',
+		// 	   method: 'post',
+		// 	   body: data}).then((response) => response.json())
+		// 				   .then((data) => console.log(data));
+
+		// GET request
+		fetch(`/rate-with-get.json?rating=${this.state.rating}`,
+			  {credentials: 'include'}).then((response) => response.json())
+									   .then((data) => console.log(data));
 	}
 
 	rate() {
-		let rating = Number(this.props.value);
-		this.setState({rating: this.props.value})
-
-		data = {'rating': this.state.rating,
-				'restaurant_id': REPLACE}
-		console.log(data);
-
-		fetch('/rate-restaurant',
-			  {method: 'post',
-			   body: JSON.stringify(data)});
+		this.setState({rating: Number(this.props.value)}, this.changeRating);
 	}
-
+	
 	render() {
+		
 		return (
-			<button className="rateButton" onClick={ this.rate }>{this.state.rating}
+			<button className="rateButton" onClick={ this.rate }>{this.props.value}
 			</button>
 			);
 	}
