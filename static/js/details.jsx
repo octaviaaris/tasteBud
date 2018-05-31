a8e11a2e4fe0ca84a0aee2be51c11ce679e53f60
Octavia Payne <oapayne@gmail.com>
2018-05-31 16:56:41 -0700

    commented out files; incorporated into DetailsAndRating.jsx

--- static/js/details.jsx
+++ static/js/details.jsx
@@ -1,41 +1,42 @@
-class RestaurantDetails extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {details: {},
-					  categories: [],
-					  userRating: "Rate below"};
-	}
+// class RestaurantDetails extends React.Component {
+// 	constructor(props) {
+// 		super(props);
+// 		this.state = {details: {},
+// 					  categories: [],
+// 					  userRating: "Rate below"};
+// 	}
 
-	componentDidMount() {
+// 	componentDidMount() {
 
-		fetch('/details.json',
-			  {credentials: 'include'}).then((response) => response.json())
-									   .then((data) => this.setState({details: data, categories: data['categories']}));
+// 		fetch('/details.json',
+// 			  {credentials: 'include'}).then((response) => response.json())
+// 									   .then((data) => this.setState({details: data, categories: data['categories']}));
 
-		fetch('/user-rating.json',
-			  {credentials: 'include'}).then((response) => response.json())
-									   .then((data) => (data) ? this.setState({userRating: '*'.repeat(data['userRating'])}) 
-									   						  : this.setState({userRating: "Changed"}));
+// 		// fetch('/user-rating.json',
+// 		// 	  {credentials: 'include'}).then((response) => response.json())
+// 		// 							   .then((data) => (data) ? this.setState({userRating: '*'.repeat(data['userRating'])}) 
+// 		// 							   						  : this.setState({userRating: "Changed"}));
+// 	}
 
-	}
+// 	render() {
 
-	render() {
+// 		const rating = this.props.rating;
 
-		return (
-			<div>
-				<h3>{this.state.details['name']}</h3>
-				Yelp: {'*'.repeat(this.state.details['yelp_rating'])} | You: {this.state.userRating}
-				<p>{'$'.repeat(this.state.details['price'])} | {this.state.categories.join(", ")}</p>
-				<p>Address:</p>
-				{this.state.details['address1']}<br/>
-				{this.state.details['city']}, {this.state.details['state']} {this.state.details['zipcode']}
-				<p><a href={this.state.details['yelp_url']} target="_blank">Go to yelp page</a></p>
-			</div>
-			)
-	}
-}
+// 		return (
+// 			<div>
+// 				<h3>{this.state.details['name']}</h3>
+// 				Yelp: {'*'.repeat(this.state.details['yelp_rating'])} | You: {rating}
+// 				<p>{'$'.repeat(this.state.details['price'])} | {this.state.categories.join(", ")}</p>
+// 				<p>Address:</p>
+// 				{this.state.details['address1']}<br/>
+// 				{this.state.details['city']}, {this.state.details['state']} {this.state.details['zipcode']}
+// 				<p><a href={this.state.details['yelp_url']} target="_blank">Go to yelp page</a></p>
+// 			</div>
+// 			)
+// 	}
+// }
 
-ReactDOM.render(
-	<RestaurantDetails />,
-	document.getElementById("details")
-);
+// ReactDOM.render(
+// 	<RestaurantDetails />,
+// 	document.getElementById("details")
+// );
--- static/js/rateButtons.jsx
+++ static/js/rateButtons.jsx
@@ -1,52 +1,57 @@
-class RateButton extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {rating: 0};
-		this.rate = this.rate.bind(this);
-		this.changeRating = this.changeRating.bind(this);
-	}
+// class RateButton extends React.Component {
+// 	constructor(props) {
+// 		super(props);
+// 		this.state = {rating: 0};
+// 		this.rate = this.rate.bind(this);
+// 	}
 
-	changeRating() {
+// 	changeRating() {
 
-		// POST request
-		// let data = new FormData();
-		// data.append("json", JSON.stringify({'rating': this.state.rating}));
+// 		// POST request
+// 		// let data = new FormData();
+// 		// data.append("json", JSON.stringify({'rating': this.state.rating}));
 
-		// fetch('/rate-restaurant.json',
-		// 	  {credentials: 'include',
-		// 	   method: 'post',
-		// 	   body: data}).then((response) => response.json())
-		// 				   .then((data) => console.log(data));
+// 		// fetch('/rate-restaurant.json',
+// 		// 	  {credentials: 'include',
+// 		// 	   method: 'post',
+// 		// 	   body: data}).then((response) => response.json())
+// 		// 				   .then((data) => console.log(data));
 
-		// GET request
-		fetch(`/rate-with-get.json?rating=${this.state.rating}`,
-			  {credentials: 'include'}).then((response) => response.json())
-									   .then((data) => console.log(data));
-	}
+// 		// GET request
+// 		fetch(`/rate-with-get.json?rating=${this.state.rating}`,
+// 			  {credentials: 'include'}).then((response) => response.json())
+// 									   .then((data) => console.log(data));
+// 	}
 
-	rate() {
-		this.setState({rating: Number(this.props.value)}, this.changeRating);
-	}
+// 	handleChange(evt) {
+// 		this.props.onRateChange(evt.target.value);
+// 	}
+
+// 	rate() {
+// 		this.setState({rating: Number(this.props.value)}, this.changeRating);
+// 	}
 	
-	render() {
+// 	render() {
 		
-		return (
-			<button className="rateButton" onClick={ this.rate }>{this.props.value}
-			</button>
-			);
-	}
+// 		const rating = this.props.rating;
 
-}
+// 		return (
+// 			<div>
+// 				<button className="rateButton" onClick={ this.rate }>{this.props.value}
+// 				</button>
+// 			</div>
+// 			);
+// 	}
+// }
 
-ReactDOM.render(
+// ReactDOM.render(
 
-	<div>
-		<RateButton value="1" />
-		<RateButton value="2" />
-		<RateButton value="3" />
-		<RateButton value="4" />
-		<RateButton value="5" />
-	</div>,
-	document.getElementById("rating")
-
-	);
+// 	<div>
+// 		<RateButton value="1" />
+// 		<RateButton value="2" />
+// 		<RateButton value="3" />
+// 		<RateButton value="4" />
+// 		<RateButton value="5" />
+// 	</div>,
+// 	document.getElementById("rating")
+// 	);
