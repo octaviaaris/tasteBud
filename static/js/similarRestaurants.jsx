c87955f3cfc7aacbbb772dfc718fe232be18d7fd
Octavia Payne <oapayne@gmail.com>
2018-06-10 21:31:58 -0700

    created new component for details page

---
+++ static/js/similarRestaurants.jsx
@@ -0,0 +1,83 @@
+class SimilarRestaurants extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {restaurants: "unchanged",
+					  currentRestaurant: "",
+					  similars: []};
+	}
+
+	createYelpStars = (rtg) => {
+		let yelp =[];
+		for (let step = 0; step < rtg; step++ ) {
+			yelp.push(<i key={step} className="fas fa-star"></i>)
+		}
+
+		return yelp;
+	}
+
+	createPriceIcons = (price) => {
+		let priceIcon = [];
+				for (let step = 0; step < price; step++ ) {
+					priceIcon.push(<i key={step} className="fas fa-dollar-sign"></i>)
+				}
+
+		return priceIcon;
+	}
+
+	createSimilarRestaurants = () => {
+		let newSimilars = [];
+		let idNum = 0;
+
+		for (let r in this.state.restaurants) {
+			let yelp = this.createYelpStars(this.state.restaurants[r].yelp_rating);
+			let price = this.createPriceIcons(this.state.restaurants[r].price);
+			newSimilars.push(
+				<div className="simContainer">
+					<a href={"/details/" + r}
+					   id={"sim" + idNum}
+					   key={idNum}
+					   className="m-auto simRestaurantBox">
+						<p className="address name">{this.state.restaurants[r].name}</p>
+						{yelp}<br/>
+						<p>{price} | {this.state.restaurants[r]['categories'].join(", ")}</p>
+						<p className="address">{this.state.restaurants[r].city}</p>
+					</a>
+				</div>
+			)
+			idNum++;
+		}
+
+		this.setState({similars: newSimilars});
+	}
+	
+
+	changeState = (data) => {
+		this.setState({restaurants: data}, this.createSimilarRestaurants);
+	}
+
+	componentDidMount() {
+		
+		fetch('/details.json',
+			  {credentials: 'include'}).then((response) => response.json())
+									   .then((data) => this.setState({currentRestaurant: data['name']}));
+
+		fetch('/similar-restaurants.json',
+			  {credentials: 'include'}).then((response) => response.json())
+									   .then((data) => this.changeState(data));
+	}
+
+	render() {
+
+		return (
+			<div className="simRestaurants">
+				<p>Other places like {this.state.currentRestaurant}:</p>
+				<div className="row simRow">{this.state.similars}</div>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(
+	<SimilarRestaurants />,
+	document.getElementById("similarRestaurants")
+);
